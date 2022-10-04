4102d4f91c60b05d2cc24bff87b5d899634e084c
aeromandy <aeromandy2016@gmail.com>
2022-10-03 22:45:16 -0300

    login e senha goodpay

---
+++ js/moduloDados.js
@@ -0,0 +1,37 @@
+/** 
+ * Dentro deste array existem usuarios com esta estrutura:
+ * id
+ * nomeUsuario
+ * senha
+ * email
+ */
+
+ const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: "jose-silva",
+        senha: "12345",
+        email: "jose.silva@gmail.com",
+    },
+    {
+        id: 2,
+        nomeUsuario: "renato3x",
+        senha: "12345",
+        email: "renato.pereira@gmail.com",
+    },
+    {
+        id: 3,
+        nomeUsuario: "bruno-dias",
+        senha: "12345",
+        email: "bruno.dias@gmail.com",
+    },
+    {
+        id: 4,
+        nomeUsuario: "pedro-viana",
+        senha: "12345",
+        email: "pedro.viana@gmail.com",
+    }
+];
+
+export default usuarios;
+/* Disponibiliza o array para ser utilizado fora do arquivo modeloDados.js */
